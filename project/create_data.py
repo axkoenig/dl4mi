bf671d9433ada961ff2508d4b3fba0d90b26c608
ga96qov <li.nguyen@tum.de>
2020-05-25 13:26:04 +0200

    Update create data.py (using 5 datasets instead of 3)

--- project/create_data.py
+++ project/create_data.py
@@ -14,15 +14,24 @@ random.seed(seed)
 MAXVAL = 255  # Range [0 255]
 
 # path to covid-19 dataset from https://github.com/ieee8023/covid-chestxray-dataset
-cohen_imgpath = 'covid-chestxray-dataset/images' 
-cohen_csvpath = 'covid-chestxray-dataset/metadata.csv'
+cohen_imgpath = '../covid-chestxray-dataset/images' 
+cohen_csvpath = '../covid-chestxray-dataset/metadata.csv'
 
-# path to covid-14 dataset from https://github.com/agchung/Figure1-COVID-chestxray-dataset
-fig1_imgpath = 'Figure1-COVID-chestxray-dataset/images'
-fig1_csvpath = 'Figure1-COVID-chestxray-dataset/metadata.csv'
+# path to covid-19 dataset from https://github.com/agchung/Figure1-COVID-chestxray-dataset
+fig1_imgpath = '../Figure1-COVID-chestxray-dataset/images'
+fig1_csvpath = '../Figure1-COVID-chestxray-dataset/metadata.csv'
+
+# path to covid-19 dataset from https://github.com/agchung/Actualmed-COVID-chestxray-dataset
+actmed_imgpath = '../Actualmed-COVID-chestxray-dataset/images'
+actmed_csvpath = '../Actualmed-COVID-chestxray-dataset/metadata.csv'
+
+# path to covid-19 dataset from https://www.kaggle.com/tawsifurrahman/covid19-radiography-database
+sirm_imgpath = '../COVID-19-Radiography-Database/COVID-19'
+sirm_imgpath = '../COVID-19-Radiography-Database/COVID-19'
+sirm_csvpath = '../COVID-19-Radiography-Database/COVID-19.metadata.xlsx'
 
 # path to https://www.kaggle.com/c/rsna-pneumonia-detection-challenge
-rsna_datapath = 'rsna-pneumonia-detection-challenge'
+rsna_datapath = '../rsna-pneumonia-detection-challenge'
 # get all the normal from here
 rsna_csvname = 'stage_2_detailed_class_info.csv' 
 # get all the 1s from here since 1 indicate pneumonia
@@ -62,43 +71,78 @@ cohen_idx_keep = cohen_csv.view.isin(views)
 cohen_csv = cohen_csv[cohen_idx_keep]
 
 fig1_csv = pd.read_csv(fig1_csvpath, encoding='ISO-8859-1', nrows=None)
-#fig1_idx_keep = fig1_csv.view.isin(views)
-#fig1_csv = fig1_csv[fig1_idx_keep]
+actmed_csv = pd.read_csv(actmed_csvpath, nrows=None)
 
-# get non-COVID19 viral, bacteria, and COVID-19 infections from covid-chestxray-dataset
+sirm_csv = pd.read_excel(sirm_csvpath)
+
+# get non-COVID19 viral, bacteria, and COVID-19 infections from covid-chestxray-dataset, figure1 and actualmed
 # stored as patient id, image filename and label
 filename_label = {'normal': [], 'pneumonia': [], 'COVID-19': []}
 count = {'normal': 0, 'pneumonia': 0, 'COVID-19': 0}
-# cohen dataset
+covid_ds = {'cohen': [], 'fig1': [], 'actmed': [], 'sirm': []}
+
 for index, row in cohen_csv.iterrows():
     f = row['finding'].split(',')[0] # take the first finding, for the case of COVID-19, ARDS
     if f in mapping: # 
         count[mapping[f]] += 1
-        entry = [str(row['patientid']), row['filename'], mapping[f], row['view']]
+        entry = [str(row['patientid']), row['filename'], mapping[f], 'cohen']
         filename_label[mapping[f]].append(entry)
-
-# figure 1 dataset        
+        if mapping[f] == 'COVID-19':
+            covid_ds['cohen'].append(str(row['patientid']))
+        
 for index, row in fig1_csv.iterrows():
     if not str(row['finding']) == 'nan':
         f = row['finding'].split(',')[0] # take the first finding
         if f in mapping: # 
             count[mapping[f]] += 1
             if os.path.exists(os.path.join(fig1_imgpath, row['patientid'] + '.jpg')):
-                entry = [row['patientid'], row['patientid'] + '.jpg', mapping[f]]
+                entry = [row['patientid'], row['patientid'] + '.jpg', mapping[f], 'fig1']
             elif os.path.exists(os.path.join(fig1_imgpath, row['patientid'] + '.png')):
-                entry = [row['patientid'], row['patientid'] + '.png', mapping[f]]
+                entry = [row['patientid'], row['patientid'] + '.png', mapping[f], 'fig1']
             filename_label[mapping[f]].append(entry)
+            if mapping[f] == 'COVID-19':
+                covid_ds['fig1'].append(row['patientid'])
 
-print('Data distribution from covid-chestxray-dataset:')
+for index, row in actmed_csv.iterrows():
+    if not str(row['finding']) == 'nan':
+        f = row['finding'].split(',')[0]
+        if f in mapping:
+            count[mapping[f]] += 1
+            entry = [row['patientid'], row['imagename'], mapping[f], 'actmed']
+            filename_label[mapping[f]].append(entry)
+            if mapping[f] == 'COVID-19':
+                covid_ds['actmed'].append(row['patientid'])
+    
+sirm = set(sirm_csv['URL'])
+cohen = set(cohen_csv['url'])
+discard = ['100', '101', '102', '103', '104', '105', 
+           '110', '111', '112', '113', '122', '123', 
+           '124', '125', '126', '217']
+
+for idx, row in sirm_csv.iterrows():
+    patientid = row['FILE NAME']
+    if row['URL'] not in cohen and patientid[patientid.find('(')+1:patientid.find(')')] not in discard:
+        count[mapping['COVID-19']] += 1
+        imagename = patientid + '.' + row['FORMAT'].lower()
+        if not os.path.exists(os.path.join(sirm_imgpath, imagename)):
+            imagename = patientid.split('(')[0] + ' ('+ patientid.split('(')[1] + '.' + row['FORMAT'].lower()
+        entry = [patientid, imagename, mapping['COVID-19'], 'sirm']
+        filename_label[mapping['COVID-19']].append(entry)
+        covid_ds['sirm'].append(patientid)
+    
+print('Data distribution from covid datasets:')
 print(count)
 
-# add covid-chestxray-dataset into COVIDx dataset
-# since covid-chestxray-dataset doesn't have test dataset
-# split into train/test by patientid
-# for COVIDx:
+# add covid-chestxray-dataset, figure1 and actualmed into COVIDx dataset
+# since these datasets don't have test dataset, split into train/test by patientid
+# for covid-chestxray-dataset:
 # patient 8 is used as non-COVID19 viral test
 # patient 31 is used as bacterial test
 # patients 19, 20, 36, 42, 86 are used as COVID-19 viral test
+# for figure 1:
+# patients 24, 25, 27, 29, 30, 32, 33, 36, 37, 38
+
+ds_imgpath = {'cohen': cohen_imgpath, 'fig1': fig1_imgpath, 'actmed': actmed_imgpath, 'sirm': sirm_imgpath}
 
 for key in filename_label.keys():
     arr = np.array(filename_label[key])
@@ -108,50 +152,58 @@ for key in filename_label.keys():
     # num_diff_patients = len(np.unique(arr[:,0]))
     # num_test = max(1, round(split*num_diff_patients))
     # select num_test number of random patients
+    # random.sample(list(arr[:,0]), num_test)
     if key == 'pneumonia':
         test_patients = ['8', '31']
     elif key == 'COVID-19':
         test_patients = ['19', '20', '36', '42', '86', 
                          '94', '97', '117', '132', 
-                         '138', '144', '150', '163', '169'] # random.sample(list(arr[:,0]), num_test)
+                         '138', '144', '150', '163', '169', '174', '175', '179', '190', '191'
+                         'COVID-00024', 'COVID-00025', 'COVID-00026', 'COVID-00027', 'COVID-00029',
+                         'COVID-00030', 'COVID-00032', 'COVID-00033', 'COVID-00035', 'COVID-00036',
+                         'COVID-00037', 'COVID-00038',
+                         'ANON24', 'ANON45', 'ANON126', 'ANON106', 'ANON67',
+                         'ANON153', 'ANON135', 'ANON44', 'ANON29', 'ANON201', 
+                         'ANON191', 'ANON234', 'ANON110', 'ANON112', 'ANON73', 
+                         'ANON220', 'ANON189', 'ANON30', 'ANON53', 'ANON46',
+                         'ANON218', 'ANON240', 'ANON100', 'ANON237', 'ANON158',
+                         'ANON174', 'ANON19', 'ANON195',
+                         'COVID-19(119)', 'COVID-19(87)', 'COVID-19(70)', 'COVID-19(94)', 
+                         'COVID-19(215)', 'COVID-19(77)', 'COVID-19(213)', 'COVID-19(81)', 
+                         'COVID-19(216)', 'COVID-19(72)', 'COVID-19(106)', 'COVID-19(131)', 
+                         'COVID-19(107)', 'COVID-19(116)', 'COVID-19(95)', 'COVID-19(214)', 
+                         'COVID-19(129)']
     else: 
         test_patients = []
     print('Key: ', key)
     print('Test patients: ', test_patients)
-
-    # we want to achieve the following folder structure:
-    # - test
-    #    - covid
-    #    - pneumonia
-    #    - healthy
-    # - train
-    #    - covid
-    #    - pneumonia
-    #    - healthy
-    if key == 'COVID-19':
-        save_folder = '/covid'
-    else:
-        save_folder = '/pneumonia'
-
     # go through all the patients
     for patient in arr:
-        if patient[0] not in patient_imgpath: # patient[0] is patientID and this patient is not in patient_imgpath dict at all
-            patient_imgpath[patient[0]] = [patient[1]] # patient[1] is image_name --> in patient_imgpath save with key patientId and value image_name
+        if patient[0] not in patient_imgpath:
+            patient_imgpath[patient[0]] = [patient[1]]
         else:
             if patient[1] not in patient_imgpath[patient[0]]:
-                patient_imgpath[patient[0]].append(patient[1]) # in case one patient has several images
+                patient_imgpath[patient[0]].append(patient[1])
             else:
                 continue  # skip since image has already been written
         if patient[0] in test_patients:
-            copyfile(os.path.join(cohen_imgpath, patient[1]), os.path.join(savepath, 'test' + save_folder, patient[1]))
+            if patient[3] == 'sirm':
+                image = cv2.imread(os.path.join(ds_imgpath[patient[3]], patient[1]))
+                gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+                patient[1] = patient[1].replace(' ', '')
+                cv2.imwrite(os.path.join(savepath, 'test', patient[1]), gray)
+            else:
+                copyfile(os.path.join(ds_imgpath[patient[3]], patient[1]), os.path.join(savepath, 'test', patient[1]))
             test.append(patient)
             test_count[patient[2]] += 1
         else:
-            if 'COVID' in patient[0]:
-                # print(patient[0])
-                copyfile(os.path.join(fig1_imgpath, patient[1]), os.path.join(savepath, 'train' + save_folder, patient[1]))
+            if patient[3] == 'sirm':
+                image = cv2.imread(os.path.join(ds_imgpath[patient[3]], patient[1]))
+                gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+                patient[1] = patient[1].replace(' ', '')
+                cv2.imwrite(os.path.join(savepath, 'train', patient[1]), gray)
             else:
-                copyfile(os.path.join(cohen_imgpath, patient[1]), os.path.join(savepath, 'train' + save_folder, patient[1]))
+                copyfile(os.path.join(ds_imgpath[patient[3]], patient[1]), os.path.join(savepath, 'train', patient[1]))
             train.append(patient)
             train_count[patient[2]] += 1
 
@@ -178,16 +230,8 @@ for key in patients.keys():
     # split by patients 
     # num_diff_patients = len(np.unique(arr))
     # num_test = max(1, round(split*num_diff_patients))
-    test_patients = np.load('rsna-pneumonia-detection-challenge/rsna_test_patients_{}.npy'.format(key)) # random.sample(list(arr), num_test), download the .npy files from the repo.
+    test_patients = np.load('rsna_test_patients_{}.npy'.format(key)) # random.sample(list(arr), num_test), download the .npy files from the repo.
     # np.save('rsna_test_patients_{}.npy'.format(key), np.array(test_patients))
-    
-    if key == "COVID-19":
-        save_folder = '/covid'
-    if key == "pneumonia":
-        save_folder = '/pneumonia'
-    else:
-        save_folder = '/healthy'
-
     for patient in arr:
         if patient not in patient_imgpath:
             patient_imgpath[patient] = [patient]
@@ -198,12 +242,12 @@ for key in patients.keys():
         pixel_array_numpy = ds.pixel_array
         imgname = patient + '.png'
         if patient in test_patients:
-            cv2.imwrite(os.path.join(savepath, 'test' + save_folder, imgname), pixel_array_numpy)
-            test.append([patient, imgname, key])
+            cv2.imwrite(os.path.join(savepath, 'test', imgname), pixel_array_numpy)
+            test.append([patient, imgname, key, 'rsna'])
             test_count[key] += 1
         else:
-            cv2.imwrite(os.path.join(savepath, 'train' + save_folder, imgname), pixel_array_numpy)
-            train.append([patient, imgname, key])
+            cv2.imwrite(os.path.join(savepath, 'train', imgname), pixel_array_numpy)
+            train.append([patient, imgname, key, 'rsna'])
             train_count[key] += 1
 
 print('test count: ', test_count)
@@ -218,7 +262,7 @@ print('Total length of test: ', len(test))
 
 # export to train and test csv
 # format as patientid, filename, label, separated by a space
-train_file = open("train_split_v3.txt","a") 
+train_file = open("train_split_v3.txt",'w') 
 for sample in train:
     if len(sample) == 4:
         info = str(sample[0]) + ' ' + sample[1] + ' ' + sample[2] + ' ' + sample[3] + '\n'
@@ -228,7 +272,7 @@ for sample in train:
 
 train_file.close()
 
-test_file = open("test_split_v3.txt", "a")
+test_file = open("test_split_v3.txt", 'w')
 for sample in test:
     if len(sample) == 4:
         info = str(sample[0]) + ' ' + sample[1] + ' ' + sample[2] + ' ' + sample[3] + '\n'
