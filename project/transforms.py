9a66b2abc8ce41f62af92d6a8444a05ff29d83fd
Alexander Koenig <awckoenig@gmail.com>
2020-05-27 15:08:25 +0200

    Fix bug in transforms.py

--- project/transforms.py
+++ project/transforms.py
@@ -8,30 +8,27 @@ class Transform:
     def __init__(self, mean, std, hparams):
         self.mean = mean
         self.std = std
-        self.hparams = hparams
 
-    def train(self):
-        return transforms.Compose(
+        self.train = transforms.Compose(
             [
                 transforms.RandomResizedCrop(
-                    self.hparams.image_size,
-                    scale=(self.hparams.aug_min_scale, self.hparams.aug_max_scale),
+                    hparams.image_size,
+                    scale=(hparams.aug_min_scale, hparams.aug_max_scale),
                 ),
                 transforms.RandomHorizontalFlip(),
-                transforms.RandomRotation(degrees=self.hparams.aug_rot),
+                transforms.RandomRotation(degrees=hparams.aug_rot),
                 transforms.ColorJitter(
-                    brightness=self.hparams.aug_bright, contrast=self.hparams.aug_cont
+                    brightness=hparams.aug_bright, contrast=hparams.aug_cont
                 ),
                 transforms.ToTensor(),
                 transforms.Normalize(self.mean, self.std),
             ]
         )
 
-    def test(self):
-        return transforms.Compose(
+        self.test = transforms.Compose(
             [
-                transforms.Resize(self.hparams.image_size),
-                transforms.CenterCrop(self.hparams.image_size),
+                transforms.Resize(hparams.image_size),
+                transforms.CenterCrop(hparams.image_size),
                 transforms.ToTensor(),
                 transforms.Normalize(self.mean, self.std),
             ]
