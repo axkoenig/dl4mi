26d6622d8048801b554cb081e92559faeda7bed7
Alexander Koenig <awckoenig@gmail.com>
2020-07-04 12:37:44 +0200

    Extract plot_dataset method

--- project/utils.py
+++ project/utils.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import torchvision.transforms as transforms
 from sklearn import metrics
 
 def calc_metrics(labels, predictions, verbose=False):
@@ -37,3 +40,35 @@ def calc_metrics(labels, predictions, verbose=False):
         print(eval_metrics)
 
     return eval_metrics
+
+def plot_dataset(dataset, MEAN, STD, n=6):
+    # retrieve random images from dataset
+    choice = np.random.randint(len(dataset), size=n)
+    subset = Subset(dataset, choice)
+    images = [x[0] for x in subset]
+    labels = [x[1] for x in subset]
+
+    # denormalize for visualization
+    denormalization = transforms.Normalize((-MEAN / STD).tolist(), (1.0 / STD).tolist())
+    images = [denormalization(i) for i in images]
+
+    # make grid and plot
+    grid = utils.make_grid(images)
+    label_string = []
+    for label in labels:
+        if "0":
+            label_string.append("normal")
+        elif "1":
+            label_string.append("pneumonia")
+        elif "2":
+            label_string.append("COVID-19")
+
+    title = "Labels are: " + str(label_string)
+    plt.figure(figsize=(15, 6))
+    plt.title(title)
+    plt.imshow(np.transpose(grid.numpy(), (1, 2, 0)))
+    plt.show()
+
+def freeze(model):
+    for param in model.parameters():
+        param.requires_grad = False
